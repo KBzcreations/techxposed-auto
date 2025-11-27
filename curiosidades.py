1e07e3cb5200898b030e357501c50a66564401d4
KBzcreations <kbz.tube@gmail.com>
2025-11-27 12:07:57 +0100

    Create curiosidades.py

---
+++ curiosidades.py
@@ -0,0 +1,11 @@
+# curiosidades.py
+tech_facts = [
+    "¿Sabías que el primer 'bug' de computadora fue una polilla real en 1947?",
+    "La contraseña más usada en el mundo sigue siendo '123456'.",
+    "El primer dominio registrado fue symbolics.com en 1985.",
+    "Hay más potencia de cálculo en tu móvil que en la nave Apolo 11.",
+    "El código de la WWW fue escrito en una computadora NeXT.",
+    "Nintendo fue fundada en 1889 haciendo cartas, no videojuegos.",
+    "El primer video de YouTube se titula 'Me at the zoo'.",
+    "El logo de Firefox no es un zorro, es un panda rojo.",
+]
